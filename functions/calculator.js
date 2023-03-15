34b910a232a6973bd943c0d46bddeef202faae4c
Vaghela Abhishek <vaghelaabhishek@gmail.com>
2023-03-16 02:29:39 +0530

    README.md added

--- functions/calculator.js
+++ functions/calculator.js
@@ -33,57 +33,57 @@ function cal(stack, currentNumber, sign) {
                stack.push(Math.pow(currentNumber, operand)); // Raise current number to the power of the inverse and add to stack
           }
      }
-}
 
 
-// This function Evaluate the string
-function calculate(expression) {
-     // Trim any whitespace from the input string
-     expression = trimSpaces(expression);
 
-     // Initialize a stack to hold numbers, a stack to hold sign pairs, and a default sign of '+'
-     let stack = [];
-     let stackSignPair = [];
-     let sign = "+";
+     // This function Evaluate the string
+     function calculate(expression) {
+          // Trim any whitespace from the input string
+          expression = trimSpaces(expression);
 
-     // Loop through each character in the expression
-     for (let i = 0; i < expression.length; i++) {
+          // Initialize a stack to hold numbers, a stack to hold sign pairs, and a default sign of '+'
+          let stack = [];
+          let stackSignPair = [];
+          let sign = "+";
 
-          // If the character is a number or decimal point, keep adding to the current number until a non-number character is encountered
-          if (!isNaN(Number(expression[i]))) {
-               let currentNumber = "";
-               while (!isNaN(Number(expression[i])) || expression[i] === ".") {
-                    currentNumber += expression[i];
-                    i++;
+          // Loop through each character in the expression
+          for (let i = 0; i < expression.length; i++) {
+
+               // If the character is a number or decimal point, keep adding to the current number until a non-number character is encountered
+               if (!isNaN(Number(expression[i]))) {
+                    let currentNumber = "";
+                    while (!isNaN(Number(expression[i])) || expression[i] === ".") {
+                         currentNumber += expression[i];
+                         i++;
+                    }
+                    i--;
+                    currentNumber = Number(currentNumber);
+
+                    // Use the current sign to perform the appropriate calculation and add to the stack
+                    cal(stack, currentNumber, sign);
+
+                    // If the character is an opening parenthesis, push the current stack and sign onto the sign pair stack and reset the current stack and sign
+               } else if (expression[i] === "(") {
+                    stackSignPair.push([stack, sign]);
+                    stack = [];
+                    sign = "+";
+
+                    // If the character is a closing parenthesis, reduce the current stack to a single number and pop the last sign pair from the sign pair stack to continue the calculation
+               } else if (expression[i] === ")") {
+                    let currentNumber = stack.reduce((acc, curr) => (acc += curr), 0);
+                    let getPair = stackSignPair.pop();
+                    [stack, sign] = getPair;
+
+                    // Use the popped sign to perform the appropriate calculation and add to the stack
+                    cal(stack, currentNumber, sign);
+
+                    // Otherwise, update the current sign
+               } else {
+                    sign = expression[i];
                }
-               i--;
-               currentNumber = Number(currentNumber);
-
-               // Use the current sign to perform the appropriate calculation and add to the stack
-               cal(stack, currentNumber, sign);
-
-               // If the character is an opening parenthesis, push the current stack and sign onto the sign pair stack and reset the current stack and sign
-          } else if (expression[i] === "(") {
-               stackSignPair.push([stack, sign]);
-               stack = [];
-               sign = "+";
-
-               // If the character is a closing parenthesis, reduce the current stack to a single number and pop the last sign pair from the sign pair stack to continue the calculation
-          } else if (expression[i] === ")") {
-               let currentNumber = stack.reduce((acc, curr) => (acc += curr), 0);
-               let getPair = stackSignPair.pop();
-               [stack, sign] = getPair;
-
-               // Use the popped sign to perform the appropriate calculation and add to the stack
-               cal(stack, currentNumber, sign);
-
-               // Otherwise, update the current sign
-          } else {
-               sign = expression[i];
           }
-     }
 
-     // Reduce the final stack to a single number and return it
-     let result = stack.reduce((acc, curr) => (acc += curr), 0);
-     return result;
-}
+          // Reduce the final stack to a single number and return it
+          let result = stack.reduce((acc, curr) => (acc += curr), 0);
+          return result;
+     }
